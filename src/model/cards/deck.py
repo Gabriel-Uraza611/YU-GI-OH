7bc1cd31701c503ebe132c11f602d8e4c915e6d3
Gabriel <gabriel.uraza@correounivalle.edu.co>
2025-12-03 20:53:01 -0500

    creacion de modulo para decks aleatorios

---
+++ src/model/cards/deck.py
@@ -0,0 +1,26 @@
+"""
+Creador de mazos aleatorios a partir de un conjunto de cartas disponibles.
+"""
+
+import numpy as np
+
+def random_deck(all_cards: dict) -> np.ndarray:
+    '''
+    Crea un mazo de cartas seleccionando aleatoriamente 40 cartas del conjunto
+    de cartas disponibles.
+
+    Args:
+        all_cards: Diccionario con todas las cartas disponibles, clave: número de carta
+
+    Returns:
+        list: Lista con las cartas seleccionadas para el mazo
+    '''
+
+    deck_size = 40
+    card_array = np.array(list(all_cards.values()))
+    deck = np.random.choice(
+        a=card_array,        # Población (el array de todas las cartas)
+        size=deck_size,      # Cantidad a seleccionar (40)
+        replace=False        # ¡Sin reemplazo!
+    )
+    return deck
